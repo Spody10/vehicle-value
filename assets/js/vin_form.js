5de890766a25799ef77960a02fbc8b0c4c17aaf1
Michael <michaelpspore@gmail.com>
2020-11-25 10:38:32 -0600

    Complete project
    both APIs work and local storage setup

--- assets/js/vin_form.js
+++ assets/js/vin_form.js
@@ -2,20 +2,12 @@ var inputElement = document.getElementById("insert-vin");
 var submitButton = document.getElementById("submit-button");
 inputElement.value = "KM8JN72DX7U587496";
 
-const inputEle = $("#insert-vin").keypress((event) => {
-  console.log("JQUERY EVENT: ", event);
-});
-//console.log("INPUT ELEMENT: ", inputEle);
-
-let inputValue = "";
-let isValidInput = false;
-
 function isValidLength(input) {
   return input.length === 17;
 }
 
 function handleInput(event) {
-  inputValue = event.target.value;
+  const inputValue = event.target.value;
   if (!isValidLength(inputValue)) {
     inputElement.style.borderColor = "red";
     inputElement.style.borderWidth = "5px";
